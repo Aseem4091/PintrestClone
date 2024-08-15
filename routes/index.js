c9ecf76c75944bdf7cdd5fb58a2613604530c1b8
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-08-15 16:24:12 -0400

    next section

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,68 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require('./users');
-const postModel = require('./posts');
-const passport=require('passport');
-const localStrategy=require("passport-local");
-passport.authenticate(new localStrategy(userModel.authenticate()));
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/register",function(req,res){
-  const { username, email, fullName } = req.body;
-const userData = new userModel({ username, email, fullName });
-userModel.register(userData,req.body.password)
-.then(function(){
-  passport.authenticate("local")(req,res,function(){
-    res.redirect("/profile");
+router.get('/login', function(req, res, next) {
+  res.render("login",{error:req.flash('error')});
+});
+
+router.get('/feed', function(req, res, next) {
+  res.render("feed");
+});
+
+router.get('/profile', isLoggedIn, async function(req, res, next) {
+  const user= await userModel.findOne({
+    username:req.session.passport.user
   })
-})
-})
-router.post("/login",passport.authenticate("local,succfunction(req,res){
   
-})
+  res.render("profile",{user});
+});
+
+router.post("/register", function(req, res) {
+  const { username, email, fullName, password } = req.body;
+  const userData = new userModel({ username, email, fullName });
+
+  userModel.register(userData, password)
+    .then(function() {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/profile");
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.redirect('/register'); // Change the redirect to the correct route for registration
+    });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login",
+  failureFlash:true
+}));
+
+router.get("/logout", function(req, res, next) {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
 module.exports = router;
